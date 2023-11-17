2c4dd038873a33065ed209054d7dc48ac2f70702
Yash Kumar <85291913+yashsaini7060@users.noreply.github.com>
2023-11-17 20:52:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-//your JS code here. If required.
+function  daysOfAYear(year){
+	if((year % 400 == 0) && (year % 100 == 0)){
+		alert(366);
+	}
+	else if ((year % 4 ==0) && (year % 100 != 0)){
+		alert(366);
+	}
+	else{
+		alert(365);
+	}
+}
