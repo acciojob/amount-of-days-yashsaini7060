800be51bb4d80b338fb05da9fd32fbd6e25657c9
Yash Kumar <85291913+yashsaini7060@users.noreply.github.com>
2023-11-30 19:23:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 function  daysOfAYear(year){
-	if((year % 400 == 0) && (year % 100 == 0)){
+	if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
+        // The year is a leap year, so it has 366 days
 		alert(366);
-	}
-	else if ((year % 4 ==0) && (year % 100 != 0)){
-		alert(366);
-	}
-	else{
-		alert(365);
-	}
+	} else {
+        // The year is not a leap year, so it has 365 days
+        alert(365)
+    }
+
 }
 var year = prompt()
 daysOfAYear(year)
